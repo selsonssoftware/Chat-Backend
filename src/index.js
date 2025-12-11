715186c3024729d830f8933dae54d7d09b60b279
selsonssoftware <165785938+selsonssoftware@users.noreply.github.com>
2025-12-11 17:31:19 +0530

    Remove MongoDB connection from index.js
    Removed MongoDB connection logic from index.js.

--- src/index.js
+++ src/index.js
@@ -34,10 +34,6 @@ app.get("/getvehicles", async (req, res) => {
     res.json({"error": "Failed to fetch vehicles"});
   }
 });
-mongoose.connect('mongodb://localhost:27017/dbname', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
-
 
 
 server.listen(PORT, () => {
