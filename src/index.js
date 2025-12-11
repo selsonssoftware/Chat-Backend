32f62db4010f7b43fe173b70edb9d1c98fb451e2
selsonssoftware <165785938+selsonssoftware@users.noreply.github.com>
2025-12-11 14:54:04 +0530

    Add endpoint to get messages from the database
    Added a new endpoint to retrieve messages from the database.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import Message from "./models/message.model.js";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 dotenv.config();
@@ -17,6 +18,10 @@ app.use(cors({ origin: ["http://localhost:5173", "https://socket-talk-client.ver
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+app.get("/getmessages", async (req, res) => {
+  const messages = await Message.find({});
+  res.json({"message" : messages});
+})
 app.get("/test", (req, res) => {
   res.json({"message":"working"})
 })
